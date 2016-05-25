22b735a94cc781c7c7f86002317093a361eddd07
Henrik Ravn <hravnx@gmail.com>
2016-05-25 15:57:37 +0200

    Added webpack development configuration files

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+
+
+module.exports = {
+  debug: true,
+
+  devtool: 'cheap-module-eval-source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+
+  noInfo: true, // set to false to see a list of every file being bundled.
+
+  entry: ['./src/index'],
+
+  target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
+
+  output: {
+    path: path.join(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'file-loader?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader?limit=10000&mimetype=image/svg+xml'},
+      {test: /\.(jpe?g|png|gif)$/i, loaders: ['file']},
+      {test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'},
+      {test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'sass?sourceMap']}
+    ]
+  }
+
+};
